a3b153c289f561630197490d12a94dfd57acee06
Fredrik Hallenberg <megahallon@gmail.com>
2020-08-24 13:52:13 +0200

    React build

---
+++ precache-manifest.e72d3d92998c0eefb4ff699c0382f386.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bed445721724a1f5b6bbc5502f5f7ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1b052bd5106546cbea3",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "69c535b6d6a872ce76ce",
+    "url": "/static/js/2.7f83dffc.chunk.js"
+  },
+  {
+    "revision": "9042964567d6b69af20246228b9a8af0",
+    "url": "/static/js/2.7f83dffc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1b052bd5106546cbea3",
+    "url": "/static/js/main.12a32ee9.chunk.js"
+  },
+  {
+    "revision": "2887a5f3e3c7c2bc4b8f",
+    "url": "/static/js/runtime-main.cf45e406.js"
+  }
+]);
