98b838667ec2eea01b2a8fe5ba6de4009550969e
Fredrik Hallenberg <megahallon@gmail.com>
2020-10-20 23:59:00 +0200

    Update

--- precache-manifest.d8cf5bb5f8ca0178291a677d508f7848.js
+++ precache-manifest.d8cf5bb5f8ca0178291a677d508f7848.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "faebe988dd194cdbe6287eeb568e2b74",
+    "revision": "9d0b74c585c5507f1d0d15fdf5ef20e5",
     "url": "/index.html"
   },
   {
-    "revision": "1f866f8316c7ed052f41",
+    "revision": "a9d09778b24f691b64c4",
     "url": "/static/css/main.5f361e03.chunk.css"
   },
   {
-    "revision": "2b3643f99d8323d1bbfc",
-    "url": "/static/js/2.2a5d5b61.chunk.js"
+    "revision": "6f93d56f381e7dcef29a",
+    "url": "/static/js/2.a5723efa.chunk.js"
   },
   {
     "revision": "96b17bdf3f21a581ffe1fdc9f41e68a8",
-    "url": "/static/js/2.2a5d5b61.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.a5723efa.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1f866f8316c7ed052f41",
-    "url": "/static/js/main.e695eff9.chunk.js"
+    "revision": "a9d09778b24f691b64c4",
+    "url": "/static/js/main.db65b863.chunk.js"
   },
   {
     "revision": "2887a5f3e3c7c2bc4b8f",
